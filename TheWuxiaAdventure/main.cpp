6e9d9d5fbb1e4f764cb8a46eb88f5e2ba3863023
giz_soft <15990305624@163.com>
2021-11-11 13:26:40 +0800

    Initial Commit

---
+++ TheWuxiaAdventure/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TheWuxiaAdventure
+//
+//  Created by  张乐平 on 2021/11/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
